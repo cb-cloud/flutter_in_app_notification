702c5094a50379340db35c44f0cd7500cc74e6ee
Kurogoma4D <highdro0479@gmail.com>
2021-05-28 14:42:10 +0900

    add: doc comments

--- lib/src/in_app_notification.dart
+++ lib/src/in_app_notification.dart
@@ -2,18 +2,56 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 
-const _alertHeight = 120.0;
-
+/// A widget for display foreground notification.
+///
+/// It is mainly intended to be inserted in the `builder` of [WidgetsApp].
+///
+/// {@tool snippet}
+/// Usage example:
+///
+/// ```dart
+/// return MaterialApp(
+///   home: Home(),
+///   builder: (context, child) => AlertNotification(
+///     safeAreaPadding: MediaQuery.of(context).viewPadding,
+///     child: child,
+///   ),
+/// );
+/// ```
+/// {@end-tool}
+///
+/// Showing and hiding notifications is managed by animation,
+/// and the process is as follows.
+///
+/// 1. Execute `show` method, start animation.
+/// 2. After the notification appears at the specified time,
+///    it is displayed for the specified `duration`.
+/// 3. After the `duration` has elapsed,
+///    play the animation in reverse and dispose the notification.
 class InAppNotification extends StatefulWidget {
+  /// Creates an in-app notification system.
+  ///
+  /// The [safeAreaPadding] must not be null.
   const InAppNotification({
     Key key,
     @required this.safeAreaPadding,
+    this.minAlertHeight = 120.0,
     this.child,
   })  : assert(safeAreaPadding != null),
         super(key: key);
 
   final Widget child;
+
+  /// The value of padding for something that narrows the view port,
+  /// like The Notch on the iPhone X family.
+  ///
+  /// Usually, this would be `viewPadding` of `MediaQueryData`.
   final EdgeInsets safeAreaPadding;
+
+  /// The value at minimal height of notification.
+  ///
+  /// The default value is 120.0.
+  final double minAlertHeight;
 
   static InAppNotificationState of(BuildContext context) =>
       context.findAncestorStateOfType<InAppNotificationState>();
@@ -39,7 +77,7 @@ class InAppNotificationState extends State<InAppNotification>
     _controller =
         AnimationController(vsync: this, duration: Duration(milliseconds: 350));
     final curve = CurvedAnimation(parent: _controller, curve: Curves.ease);
-    _initialPosition = -_alertHeight - widget.safeAreaPadding.top;
+    _initialPosition = -widget.minAlertHeight - widget.safeAreaPadding.top;
     _alertAnimation = Tween(begin: _initialPosition, end: 0.0).animate(curve);
     super.initState();
   }
