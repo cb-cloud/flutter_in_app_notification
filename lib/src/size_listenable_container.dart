81dd0c007df1868c32eb48a03950e72c21ae36cf
Kurogoma4D <highdro0479@gmail.com>
2021-06-15 17:52:24 +0900

    add: SizeListenableContainer

---
+++ lib/src/size_listenable_container.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/rendering.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:flutter/widgets.dart';
+
+// reference: https://qiita.com/najeira/items/0ff716971184042b1434
+
+typedef SizeChangedCallback = void Function(Size size);
+
+class SizeListenableContainer extends SingleChildRenderObjectWidget {
+  const SizeListenableContainer({
+    Key? key,
+    required Widget child,
+    required this.onSizeChanged,
+  }) : super(key: key, child: child);
+
+  final SizeChangedCallback onSizeChanged;
+
+  @override
+  _SizeListenableRenderObject createRenderObject(BuildContext context) {
+    return _SizeListenableRenderObject(onSizeChanged: onSizeChanged);
+  }
+}
+
+class _SizeListenableRenderObject extends RenderProxyBox {
+  _SizeListenableRenderObject({
+    RenderBox? child,
+    required this.onSizeChanged,
+  }) : super(child);
+
+  final SizeChangedCallback onSizeChanged;
+
+  Size? _oldSize;
+
+  @override
+  void performLayout() {
+    super.performLayout();
+
+    if (size != _oldSize) {
+      _oldSize = size;
+      _callback(size);
+    }
+  }
+
+  void _callback(Size size) {
+    SchedulerBinding.instance!.addPostFrameCallback((_) {
+      onSizeChanged(size);
+    });
+  }
+}
